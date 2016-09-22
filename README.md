f2390dc7d6a262e43c8fdb35a2a723a6a7677d3a
GrandonBroseph <grandonbroseph@gmail.com>
2016-09-21 20:11:59 -0700

    Add animations, reset button

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Multiplayer chess app made with WebSockets.
 
 ...Just kidding, I haven't added sockets quite yet.
+
+[View the demo via `gh-pages`.](https://grandonbroseph.github.io/chess/)
---
+++ src/js/dom.js
@@ -0,0 +1,29 @@
+module.exports = (function() {
+    var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    function addEvent(type, callback) {
+        if (!this) return;
+        if (this.addEventListener) {
+            this.addEventListener(type, callback, false);
+        } else if (this.attachEvent) {
+            this.attachEvent("on" + type, callback);
+        } else {
+            this["on" + type] = callback;
+        }
+    }
+    function removeEvent(type, callback) {
+        if (!this) return;
+        if (this.removeEventListener) {
+            this.removeEventListener(type, callback, false);
+        } else if (this.detachEvent) {
+            this.detachEvent("on" + type, callback);
+        } else {
+            this["on" + type] = null;
+        }
+    }
+    mobile && document.body.classList.add("mobile");
+    return {
+        mobile: mobile,
+        addEvent: addEvent,
+        removeEvent: removeEvent
+    };
+})();
